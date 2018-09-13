6b889b7a6ec76b3977a70faf45580ad4aed81cee
james <james.speirs@gmail.com>
2018-09-13 16:46:32 -0600

    update link tests

--- test/definition.links.test.js
+++ test/definition.links.test.js
@@ -21,8 +21,150 @@ const Link          = require('../bin/definition-validators/link');
 
 describe('definitions/link', () => {
 
-    it('todo', () => {
-        throw Error('TODO');
+    describe('description', () => {
+
+        it('can be a string', () => {
+            const [ err ] = definition(3, Link, {
+                description: ''
+            });
+            expect(err).to.be.undefined;
+        });
+
+        it('must be a string', () => {
+            const [ err ] = definition(3, Link, {
+                description: 1
+            });
+            expect(err).to.match(/Value must be a string/);
+        });
+
+    });
+
+    describe('operationId', () => {
+
+        it('can be a string', () => {
+            const [ err ] = definition(3, Link, {
+                operationId: 'a'
+            });
+            expect(err).to.be.undefined;
+        });
+
+        it('must be a string', () => {
+            const [ err ] = definition(3, Link, {
+                operationId: 1
+            });
+            expect(err).to.match(/Value must be a string/);
+        });
+
+        it('is mutually exclusive of operationRef', () => {
+            const [ err ] = definition(3, Link, {
+                operationId: '',
+                operationRef: ''
+            });
+            expect(err).to.match(/Must not define both operationId and operationRef/);
+        });
+
+    });
+
+    describe('operationRef', () => {
+
+        it('can be a string', () => {
+            const [ err ] = definition(3, Link, {
+                operationRef: 'a'
+            });
+            expect(err).to.be.undefined;
+        });
+
+        it('must be a string', () => {
+            const [ err ] = definition(3, Link, {
+                operationRef: 1
+            });
+            expect(err).to.match(/Value must be a string/);
+        });
+
+        it('is mutually exclusive of operationRef', () => {
+            const [ err ] = definition(3, Link, {
+                operationId: '',
+                operationRef: ''
+            });
+            expect(err).to.match(/Must not define both operationId and operationRef/);
+        });
+
+    });
+
+    describe('parameters', () => {
+
+        it('can be an object', () => {
+            const [ err ] = definition(3, Link, {
+                parameters: {}
+            });
+            expect(err).to.be.undefined;
+        });
+
+        it('must be an object', () => {
+            const [ err ] = definition(3, Link, {
+                parameters: []
+            });
+            expect(err).to.match(/Value must be a plain object/);
+        });
+
+        it('can have any type of value', () => {
+            const [ err ] = definition(3, Link, {
+                parameters: {
+                    a: 1,
+                    b: '2',
+                    c: true,
+                    d: {},
+                    f: []
+                }
+            });
+            expect(err).to.be.undefined;
+        });
+
+    });
+
+    describe('requestBody', () => {
+
+        it('can be a string', () => {
+            const [ err ] = definition(3, Link, {
+                requestBody: 'a'
+            });
+            expect(err).to.be.undefined;
+        });
+
+        it('can be a number', () => {
+            const [ err ] = definition(3, Link, {
+                requestBody: 1
+            });
+            expect(err).to.be.undefined;
+        });
+
+        it('can be an object', () => {
+            const [ err ] = definition(3, Link, {
+                requestBody: {}
+            });
+            expect(err).to.be.undefined;
+        });
+
+    });
+
+    describe('server', () => {
+
+        it('can be a server definition', () => {
+            const [ err ] = definition(3, Link, {
+                server: {
+                    url: 'a'
+                }
+            });
+            expect(err).to.be.undefined;
+        });
+
+        it('must be a server definition', () => {
+            const [ err ] = definition(3, Link, {
+                server: {}
+            });
+            expect(err).to.match(/Missing required property: url/);
+        });
+
     });
 
 });
