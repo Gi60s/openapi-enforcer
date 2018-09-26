7917bfee644c8cec79f27310d01ce3ccdf4e252c
james <james.speirs@gmail.com>
2018-09-25 18:05:56 -0600

    start writing Parameter tests

--- test/components.parameter.test.js
+++ test/components.parameter.test.js
@@ -15,128 +15,44 @@
  *    limitations under the License.
  **/
 'use strict';
-
-
-const Exception     = require('../bin/exception');
+const definition    = require('../bin/definition-validator').normalize;
 const expect        = require('chai').expect;
-const Parameter     = require('../bin/components/parameter');
-const util          = require('../bin/util');
+const Parameter     = require('../bin/definition-validators/parameter');
 
-describe.skip('components/parameter', () => {
+describe.only('components/parameter', () => {
 
-    describe('definition', () => {
+    describe('constructor', () => {
 
-        it('accepts valid definition', () => {
-            const { exception } = getParameter(2, { in: 'path', required: true, name: 'x', type: 'string' });
-            expect(exception).to.be.false;
+        it('defines schema for v2', () => {
+            const [ , def ] = definition(2, Parameter, {
+                name: 'hi',
+                in: 'query',
+                type: 'string'
+            });
+            expect(def.schema.type).to.equal('string');
         });
 
-        describe('"name" property', () => {
-
-            it('requires "name" property', () => {
-                const { exception } = getParameter(2, { in: 'path', required: true, type: 'string' });
-                expect(exception).to.match(/Missing required property: name/);
-            });
-
-            it('requires "name" property to be a non-empty string', () => {
-                const { exception } = getParameter(2, { in: 'path', required: true, name: 1, type: 'string' });
-                expect(exception).to.match(/Value for property "name" must be a string/);
-            });
-
-        });
-
-        describe('"in" property', () => {
-
-            it('requires "in" property', () => {
-                const { exception } = getParameter(2, { required: true, name: 'x', type: 'string' });
-                expect(exception).to.match(/Missing required property: in/);
-            });
-
-            it('requires valid "in" property', () => {
-                const { exception } = getParameter(2, { in: 'foo', required: true, name: 'x', type: 'string' });
-                expect(exception).to.match(/Property "in" has invalid value/);
-            });
-
-        });
-
-        describe('"required" property', () => {
-
-            it('requires that "required" be a boolean', () => {
-                const { exception } = getParameter(2, { in: 'query', required: 1, name: 'x', type: 'string' });
-                expect(exception).to.match(/Value for property "required" must be a boolean/);
-            });
-
-            it('requires that "required" be true when "in" is "path"', () => {
-                const { exception } = getParameter(2, { in: 'path', required: false, name: 'x', type: 'string' });
-                expect(exception).to.match(/Value must be true/);
-            });
-
-            it('allows "required" to be false for non "path"', () => {
-                const { exception } = getParameter(2, { in: 'query', required: false, name: 'x', type: 'string' });
-                expect(exception).to.be.false;
-            });
-
-            it('defaults to false', () => {
-                const { parameter } = getParameter(2, { in: 'query', name: 'x', type: 'string' });
-                expect(parameter.required).to.be.false;
-            });
-
-        });
-
-        describe('"allowEmptyValue" property', () => {
-
-            it('is valid for "query"', () => {
-                const { exception } = getParameter(2, { in: 'query', name: 'x', type: 'string', allowEmptyValue: true });
-                expect(exception).to.be.false;
-            });
-
-            it('is not valid for "path"', () => {
-                const { exception } = getParameter(2, { in: 'path', required: true, name: 'x', type: 'string', allowEmptyValue: true });
-                expect(exception).to.match(/Property not allowed: allowEmptyValue/);
-            });
-
-            it('defaults to false', () => {
-                const { parameter } = getParameter(2, { in: 'query', name: 'x', type: 'string' });
-                expect(parameter.allowEmptyValue).to.be.false;
-            });
-
-        });
-
-        describe('v2', () => {
-
-            it('will accept array items', () => {
-                const { exception } = getParameter(2, { in: 'query', name: 'x', type: 'array', items: { type: 'string' }, collectionFormat: 'csv' });
-                expect(exception).to.be.false;
-            });
-
-            it('will accept nested array items', () => {
-                const { exception } = getParameter(2, {
-                    in: 'query',
-                    name: 'x',
-                    type: 'array',
-                    items: {
-                        type: 'array',
-                        collectionFormat: 'pipes',
-                        items: {
-                            type: 'array',
-                            collectionFormat: 'tsv',
-                            items: {
-                                type: 'number'
-                            }
-                        }
+        it('defines schema for v3 with content', () => {
+            const [ , def ] = definition(3, Parameter, {
+                name: 'hi',
+                in: 'query',
+                content: {
+                    'application/json': {
+                        schema: { type: 'string' }
                     }
-                });
-                expect(exception).to.be.false;
+                }
             });
+            expect(def.schema.type).to.equal('string');
+        })
 
+    });
+
+    describe('parse', () => {
+
+        it('todo', () => {
+            throw Error('todo')
         });
 
     });
 
 });
-
-function getParameter(version, definition) {
-    const exception = Exception('');
-    const parameter = new Parameter({ version }, exception, definition, new WeakMap());
-    return { exception: exception.hasException ? exception: false, parameter };
-}
