3ba31bd8ad0596376090f5b30bfda384591c04d3
james <james.speirs@gmail.com>
2018-09-27 10:49:44 -0600

    create tests

---
+++ test/components.operation.test.js
@@ -0,0 +1,67 @@
+/**
+ *  @license
+ *    Copyright 2018 Brigham Young University
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ **/
+'use strict';
+const definition    = require('../bin/definition-validator').normalize;
+const expect        = require('chai').expect;
+const Path          = require('../bin/definition-validators/path');
+
+describe.only('components/operation', () => {
+
+    describe('constructor', () => {
+        let def;
+
+        before(() => {
+            const result = definition(2, Path, {
+                get: {
+                    parameters: [
+                        { name: 'b', in: 'path', type: 'number', required: true },
+                        { name: 'c', in: 'query', type: 'number', required: true }
+                    ],
+                    responses: {
+                        200: { description: '' }
+                    }
+                },
+                parameters: [
+                    { name: 'a', in: 'path', type: 'string', required: true },
+                    { name: 'b', in: 'path', type: 'string', required: true },
+                    { name: 'b', in: 'query', type: 'string' }
+                ]
+            });
+            def = result.value;
+        });
+
+        it('merges parameters from path and operation', () => {
+            const parameters = def.get.parameters;
+            expect(parameters.length).to.equal(4);
+            expect(parameters.filter(p => p.in === 'path').length).to.equal(2);
+            expect(parameters.filter(p => p.in === 'query').length).to.equal(2);
+        });
+
+        it('creates a parameters map', () => {
+            const map = def.get.parametersMap;
+            const keys = Object.keys(map);
+            keys.sort();
+            expect(keys).to.deep.equal(['path', 'query']);
+            expect(map.path).to.haveOwnProperty('a');
+            expect(map.path).to.haveOwnProperty('b');
+            expect(map.query).to.haveOwnProperty('b');
+            expect(map.query).to.haveOwnProperty('c');
+        });
+
+    });
+
+});
