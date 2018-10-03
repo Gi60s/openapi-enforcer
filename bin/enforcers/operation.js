631d711efe63eb5323dc8493719d3c91916e43c3
james <james.speirs@gmail.com>
2018-10-02 18:22:44 -0600

    refactor to new logic

--- bin/enforcers/operation.js
+++ bin/enforcers/operation.js
@@ -17,11 +17,16 @@
 'use strict';
 const Exception     = require('../exception');
 const Parameter     = require('./parameter');
+const Result        = require('../result');
 const util          = require('../util');
+
+const store = new WeakMap();
 
 module.exports = OperationEnforcer;
 
-function OperationEnforcer({ definition, parent }) {
+function OperationEnforcer(data) {
+    store.set(this, data);
+    const { definition, parent } = data;
     Object.assign(this, definition);
 
     const parameters = [];
@@ -41,7 +46,7 @@ function OperationEnforcer({ definition, parent }) {
                 if (!parametersProcessed) {
 
                     // build the parameters map
-                    buildParametersMap(parametersMap, parent.parameters);
+                    if (parent) buildParametersMap(parametersMap, parent.parameters);
                     buildParametersMap(parametersMap, definition.parameters);
 
                     // overwrite the parameters array
@@ -67,69 +72,86 @@ function OperationEnforcer({ definition, parent }) {
  * @param {object} request
  * @param {string|object} request.body The request body
  * @param {Object<string,string>} [request.headers={}] The request headers
- * @param {string} [request.path='/'] The path and query string
+ * @param {string} request.path The path and query string
  * @param {object} [options]
+ * @param {boolean} [options.allowOtherFormDataParameters=false] Allow form data that is not specified in the OAS document
  * @param {boolean} [options.allowOtherQueryParameters=false] Allow query parameters that are not specified in the OAS document
  * @param {boolean} [options.allowOtherCookieParameters=true] Allow cookies that are not specified in the OAS document
  * @param {boolean} [options.bodyDeserializer] A function to call to deserialize the body into it's expected type.
+ * @param {Object<string,string>} [options.pathParametersValueMap] A map of the already parsed out path parameters.
  */
-OperationEnforcer.prototype.request = function(request, options) {
-    // validate request parameter and properties
+OperationEnforcer.prototype.request = async function (request, options) {
+    const { major } = store.get(this);
+
+    // validate input parameters
+    if (!request || typeof request !== 'object') throw Error('Invalid request. Expected a non-null object. Received: ' + request);
+    if (typeof request.path !== 'string') throw Error('Invalid request path. Expected a string. Received: ' + request.path);
     if (request.hasOwnProperty('body') && !(typeof request.body === 'string' || typeof request.body === 'object')) throw Error('Invalid body provided');
-    if (request.hasOwnProperty('cookies') && typeof request.cookies !== 'string') throw Error('Invalid request cookies. Expected a string.');
     if (request.hasOwnProperty('headers') && !util.isObjectStringMap(request.headers)) throw Error('Invalid request headers. Expected an object with string keys and string values');
     if (request.hasOwnProperty('path') && typeof request.path !== 'string') throw Error('Invalid request path. Expected a string');
 
+    if (!options) options = {};
+    if (typeof options !== 'object') throw Error('Invalid options. Expected an object. Received: ' + options);
+    if (!options.hasOwnProperty('allowOtherFormDataParameters')) options.allowOtherFormDataParameters = false;
+    if (!options.hasOwnProperty('allowOtherQueryParameters')) options.allowOtherQueryParameters = false;
+    if (!options.hasOwnProperty('allowOtherCookieParameters')) options.allowOtherCookieParameters = true;
+
     // build request objects
-    const [ path, query ] = request.path.split('?');
-    const req = { path, query };
-    req.header = request.headers ? util.lowerCaseObjectProperties(request.headers) : {};
+    const pathParts = request.path.split('?');
+    const path = pathParts[0];
+    const query = pathParts[1] ? decodeURIComponent(pathParts[1]) : '';
+    const req = {
+        header: request.headers ? util.lowerCaseObjectProperties(request.headers) : {},
+        path: util.edgeSlashes(path, true, false),
+        query: query ? util.parseQueryString(query) : {}
+    };
     req.cookie = req.header.cookies || '';
     delete req.header.cookies;
-
-    // normalize options
-    options = Object.assign({ allowOtherQueryParameters: false }, options);
 
     const exception = Exception('Request has one or more errors');
     exception.statusCode = 400;
 
     const parameters = this.parametersMap;
 
-
+    const result = {
+        cookie: {},
+        header: {},
+        path: {},
+        query: {}
+    };
     ['cookie', 'header', 'path', 'query'].forEach(at => {
+        const allowUnknownParameters = options['allowOther' + util.ucFirst(at) + 'Parameters'] || false;
+        const child = exception.nest('In ' + at + ' parameters');
+        const input = req[at];
+        const missingRequired = [];
+
+        const unknownParameters = allowUnknownParameters || at === 'path'
+            ? []
+            : Object.keys(input);
+
         if (parameters[at]) {
-            const child = exception.nest('In ' + at + ' parameters');
-            const input = req[at];
             const output = {};
-            const missingRequired = [];
 
-            // TODO: allowOtherCookieParameters
-            const unknownParameters = at === 'query' && !options.allowOtherQueryParameters
-                ? Object.keys(input)
-                : [];
-
-            Object.keys(parameters[at].map).forEach(name => {
-                // TODO: working here
-                const key = at === 'header' ? headerNamesMap[name] : name;
-                const parameter = parameters[at].map[name];
+            Object.keys(parameters[at]).forEach(key => {
+                const parameter = parameters[at][key];
                 const type = parameter.schema && parameter.schema.type;
                 if (input.hasOwnProperty(key)) {
                     util.arrayRemoveItem(unknownParameters, key);
                     const data = (at === 'query' || at === 'cookie')
-                        ? parameter.parse(queryString, input)
+                        ? parameter.parse(query, input)
                         : parameter.parse(input[key]);
-                    processInput(child.at(key), parameter, data, v => output[key] = v);
+                    deserializeAndValidate(child.at(key), parameter, data, v => output[key] = v);
 
                 } else if (parameter.in === 'query' && parameter.style === 'form' && parameter.explode && type === 'object') {
-                    const data = parameter.parse(queryString, input);
-                    processInput(null, parameter, data, value => {
+                    const data = parameter.parse(query, input);
+                    deserializeAndValidate(null, parameter, data, value => {
                         Object.keys(value).forEach(key => util.arrayRemoveItem(unknownParameters, key));
                         output[key] = value;
                     })
 
                 } else if (parameter.in === 'query' && parameter.style === 'deepObject' && type === 'object') {
-                    const data = parameter.parse(queryString, input);
-                    processInput(child.at(key), parameter, data, value => {
+                    const data = parameter.parse(query, input);
+                    deserializeAndValidate(child.at(key), parameter, data, value => {
                         Object.keys(value).forEach(k => util.arrayRemoveItem(unknownParameters, key + '[' + k + ']'));
                         output[key] = value;
                     });
@@ -139,22 +161,24 @@ OperationEnforcer.prototype.request = function(request, options) {
                 }
             });
 
-            req[at] = output;
-
-            // add exception for any unknown query parameters
-            if (unknownParameters.length) {
-                child('Received unexpected parameter' +
-                    (unknownParameters.length === 1 ? '' : 's') + ': ' +
-                    unknownParameters.join(', '));
-            }
-
-            //
-            if (missingRequired.length) {
-                child('Missing required parameter' + (missingRequired.length > 1 ? 's' : '') +
-                    ': ' + missingRequired.join(', '));
-            }
+            result[at] = output;
         }
-    })
+
+        // add exception for any unknown query parameters
+        if (unknownParameters.length) {
+            child('Received unexpected parameter' +
+                (unknownParameters.length === 1 ? '' : 's') + ': ' +
+                unknownParameters.join(', '));
+        }
+
+        //
+        if (missingRequired.length) {
+            child('Missing required parameter' + (missingRequired.length > 1 ? 's' : '') +
+                ': ' + missingRequired.join(', '));
+        }
+    });
+
+    return new Result(exception, result);
 };
 
 function buildParametersMap(map, parameters) {
@@ -163,5 +187,15 @@ function buildParametersMap(map, parameters) {
             if (!map.hasOwnProperty(parameter.in)) map[parameter.in] = {};
             map[parameter.in][parameter.name] = parameter;
         })
+    }
+}
+
+function deserializeAndValidate(exception, parameter, data, success) {
+    if (!data.error) data = parameter.schema.deserialize(data.value);
+    if (!data.error) data.error = parameter.schema.validate(data.value);
+    if (data.error) {
+        if (exception) exception(data.error);
+    } else {
+        success(util.convertEmptyValues(data.value));
     }
 }
