1cba8ab02d1ad1bbc13fb6c835fe1ce72f22e1c2
james <james.speirs@gmail.com>
2018-10-02 18:24:04 -0600

    working on updating tests

--- test/components.operation.test.js
+++ test/components.operation.test.js
@@ -17,6 +17,7 @@
 'use strict';
 const definition    = require('../bin/definition-validator').normalize;
 const expect        = require('chai').expect;
+const Operation     = require('../bin/definition-validators/operation');
 const Path          = require('../bin/definition-validators/path');
 
 describe('components/operation', () => {
@@ -64,4 +65,350 @@ describe('components/operation', () => {
 
     });
 
+    describe.only('request', () => {
+
+        describe('in body', () => {
+
+            describe('string', () => {
+                const def = {
+                    parameters: [{ name: 'body', in: 'body', schema: { type: 'string' }}],
+                    responses: { 200: { description: '' } }
+                };
+
+                it('is allowed', () => {
+                    const [ , operation ] = definition(2, Operation, def);
+                    return operation.request({ path: '/', body: 'hello' })
+                        .then(req => {
+                            expect(req.body).to.equal('hello');
+                        });
+                });
+
+                it('is validated', () => {
+                    const [ , operation ] = definition(2, Operation, def);
+                    return operation.request({ body: 'hello' })
+                        .then(shouldHaveRejected, err => {
+                            expect(err.code).to.equal(400);
+                        });
+                });
+
+            });
+
+            describe('datetime', () => {
+
+                it('is allowed', () => {
+
+                });
+
+                it('is validated', () => {
+
+                });
+
+            });
+
+            describe('number', () => {
+
+                it('is allowed', () => {
+
+                });
+
+                it('is validated', () => {
+
+                });
+
+            });
+
+        });
+
+        describe('in cookie', () => {
+
+        });
+
+        describe('in formData', () => {
+
+        });
+
+        describe('in header', () => {
+
+        });
+
+        describe('in path', () => {
+
+        });
+
+        describe.only('in query', () => {
+
+            it('overwrites duplicate parameters for non-collection', () => {
+                const def = {
+                    parameters: [{ name: 'x', in: 'query', type: 'number' }],
+                    responses: { 200: { description: '' } }
+                };
+                const [ , operation ] = definition(2, Operation, def);
+                return operation.request({ path: '/?x=1&x=2' })
+                    .then(([ err, req ]) => {
+                        expect(req.query.x).to.equal(2);
+                    });
+            });
+
+            it('does allow non defined parameter w/ allowOtherQueryParameters', () => {
+                const def = {
+                    parameters: [],
+                    responses: { 200: { description: '' } }
+                };
+                const [ , operation ] = definition(2, Operation, def);
+                return operation.request({ path: '/?x=1' }, { allowOtherQueryParameters: true })
+                    .then(([ , req ]) => {
+                        expect(req.query).to.deep.equal({});
+                    });
+            });
+
+            it('does not allow non defined parameter w/o allowOtherQueryParameters', () => {
+                const def = {
+                    parameters: [],
+                    responses: { 200: { description: '' } }
+                };
+                const [ , operation ] = definition(2, Operation, def);
+                return operation.request({ path: '/?x=1' }, { allowOtherQueryParameters: false })
+                    .then(([ err ]) => {
+                        expect(err).to.match(/Received unexpected parameter: x/);
+                    });
+            });
+
+            describe('v2 array', () => {
+                let def;
+
+                beforeEach(() => {
+                    def = {
+                        parameters: [{ name: 'x', in: 'query', type: 'array', items: { type: 'number' } }],
+                        responses: { 200: { description: '' } }
+                    };
+                });
+
+                it('can deserialize a csv collection', () => {
+                    def.parameters[0].collectionFormat = 'csv';
+                    const [ , operation ] = definition(2, Operation, def);
+                    return operation.request({ path: '/?x=1,2,3' })
+                        .then(([ , req ]) => {
+                            expect(req.query.x).to.deep.equal([ 1, 2, 3 ]);
+                        });
+                });
+
+                it('can deserialize an ssv collection', () => {
+                    def.parameters[0].collectionFormat = 'ssv';
+                    const [ , operation ] = definition(2, Operation, def);
+                    return operation.request({ path: '/?x=1%202%203' })
+                        .then(([ , req ]) => {
+                            expect(req.query.x).to.deep.equal([ 1, 2, 3 ]);
+                        });
+                });
+
+                it('can deserialize a tsv collection', () => {
+                    def.parameters[0].collectionFormat = 'tsv';
+                    const [ , operation ] = definition(2, Operation, def);
+                    return operation.request({ path: '/?x=1%092%093' })
+                        .then(([ , req ]) => {
+                            expect(req.query.x).to.deep.equal([ 1, 2, 3 ]);
+                        });
+                });
+
+                it('can deserialize a pipes collection', () => {
+                    def.parameters[0].collectionFormat = 'pipes';
+                    const [ , operation ] = definition(2, Operation, def);
+                    return operation.request({ path: '/?x=1|2|3' })
+                        .then(([ , req ]) => {
+                            expect(req.query.x).to.deep.equal([ 1, 2, 3 ]);
+                        });
+                });
+
+                it('can deserialize a multi collection', () => {
+                    def.parameters[0].collectionFormat = 'multi';
+                    const [ , operation ] = definition(2, Operation, def);
+                    return operation.request({ path: '/?x=1&x=2&x=3' })
+                        .then(([ , req ]) => {
+                            expect(req.query.x).to.deep.equal([ 1, 2, 3 ]);
+                        });
+                });
+
+            });
+
+            describe('v3 array', () => {
+                let def;
+
+                beforeEach(() => {
+                    def = {
+                        parameters: [
+                            { name: 'color', in: 'query' }
+                        ],
+                        responses: { 200: { description: '' } }
+                    };
+                });
+
+                describe('default style (form)', () => {
+
+                    it('empty', () => {
+                        def.parameters[0].allowEmptyValue = true;
+                        def.parameters[0].schema = { type: 'string' };
+                        const [ , operation ] = definition(3, Operation, def);
+                        return operation.request({ path: '/?color=' })
+                            .then(([ err, req ]) => {
+                                expect(req.query).to.haveOwnProperty('color');
+                                expect(req.query.color).to.equal(undefined);
+                            });
+                    });
+
+                    it('string', () => {
+                        def.parameters[0].schema = { type: 'string' };
+                        const [ err, operation ] = definition(3, Operation, def);
+                        return operation.request({ path: '/?x=1&color=red&y=2' }, { allowOtherQueryParameters: true })
+                            .then(([ err, req ]) => {
+                                expect(req.query.color).to.equal('red');
+                            });
+                    });
+
+                    it('array', () => {
+                        def.parameters[0].explode = false;
+                        def.parameters[0].schema = { type: 'array', items: { type: 'string' } };
+                        const [ err, operation ] = definition(3, Operation, def);
+                        return operation.request({ path: '/?color=orange&color=blue,black,brown' })
+                            .then(([ err, req ]) => {
+                                expect(req.query.color).to.deep.equal(['blue', 'black', 'brown']);
+                            });
+                    });
+
+                    it('array (default exploded)', () => {
+                        def.parameters[0].schema = { type: 'array', items: { type: 'string' } };
+                        const [ err, operation ] = definition(3, Operation, def);
+                        return operation.request({ path: '/?color=blue&color=black&x=1&color=brown' }, { allowOtherQueryParameters: true })
+                            .then(([ err, req ]) => {
+                                expect(req.query.color).to.deep.equal(['blue', 'black', 'brown']);
+                            });
+                    });
+
+                    it('object (default exploded)', () => {
+                        def.parameters[0].schema = {
+                            type: 'object',
+                            properties: {
+                                R: { type: 'number' },
+                                G: { type: 'number' },
+                                B: { type: 'number' }
+                            }
+                        };
+                        const [ err, operation ] = definition(3, Operation, def);
+                        return operation.request({ path: '/?' + encodeURIComponent('R=100&G=200&B=150') })
+                            .then(([ err, req ]) => {
+                                expect(req.query.color).to.deep.equal({ R: 100, G: 200, B: 150 });
+                            });
+                    });
+
+                    it('object', () => {
+                        def.parameters[0].explode = false;
+                        def.parameters[0].schema = {
+                            type: 'object',
+                            properties: {
+                                R: { type: 'number' },
+                                G: { type: 'number' },
+                                B: { type: 'number' }
+                            }
+                        };
+                        const [ , operation ] = definition(3, Operation, def);
+                        return operation.request({ path: '/?color=R,100,G,200,B,150' })
+                            .then(([ err, req ]) => {
+                                expect(req.query.color).to.deep.equal({ R: 100, G: 200, B: 150 });
+                            });
+                    });
+
+                });
+
+                describe('spaceDelimited', () => {
+
+                    it('array', () => {
+                        const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'spaceDelimited', schema: { type: 'array' }}});
+                        const instance = new enforcer(schema2, {});
+                        const req = request({ path: '/?color=blue%20black%20brown'});
+                        const params = instance.request(req);
+                        expect(params.query.color).to.deep.equal(['blue', 'black', 'brown']);
+                    });
+
+                    it('object', () => {
+                        const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'spaceDelimited' }});
+                        const instance = new enforcer(schema2, {});
+                        const req = request({ path: '/?color=R%20100%20G%20200%20B%20150'});
+                        const params = instance.request(req);
+                        expect(params.query.color).to.deep.equal({ R: 100, G: 200, B: 150 });
+                    });
+
+                });
+
+                describe('pipeDelimited\t', () => {
+
+                    it('array', () => {
+                        const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'pipeDelimited', schema: { type: 'array' }}});
+                        const instance = new enforcer(schema2, {});
+                        const req = request({ path: '/?color=blue|black|brown'});
+                        const params = instance.request(req);
+                        expect(params.query.color).to.deep.equal(['blue', 'black', 'brown']);
+                    });
+
+                    it('object', () => {
+                        const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'pipeDelimited' }});
+                        const instance = new enforcer(schema2, {});
+                        const req = request({ path: '/?color=R|100|G|200|B|150'});
+                        const params = instance.request(req);
+                        expect(params.query.color).to.deep.equal({ R: 100, G: 200, B: 150 });
+                    });
+
+                });
+
+                it('cannot use matrix style', () => {
+                    const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'matrix' } });
+                    const req = request({ path: '/?color=' });
+                    const instance = new enforcer(schema2, {});
+                    expect(() => instance.request(req)).to.throw(/matrix style/);
+                });
+
+                it('cannot use label style', () => {
+                    const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'label' } });
+                    const req = request({ path: '/?color=' });
+                    const instance = new enforcer(schema2, {});
+                    expect(() => instance.request(req)).to.throw(/label style/);
+                });
+
+                it('cannot use simple style', () => {
+                    const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'simple' } });
+                    const req = request({ path: '/?color=' });
+                    const instance = new enforcer(schema2, {});
+                    expect(() => instance.request(req)).to.throw(/simple style/);
+                });
+
+                it('can use deepObject style', () => {
+                    const schema2 = modSchema(schema, { 'paths./.parameters.1': { style: 'deepObject' } });
+                    const instance = new enforcer(schema2, {});
+                    const req = request({ path: '/?color[R]=100&color[G]=200&color[B]=150'});
+                    const params = instance.request(req);
+                    expect(params.query.color).to.deep.equal({ R: 100, G: 200, B: 150 });
+                });
+
+            });
+
+        });
+
+    });
+
 });
+
+function modSchema(source, mods) {
+    const definition = copy(source);
+    Object.keys(mods).forEach(path => {
+        const paths = path.split('.');
+        let obj = definition;
+        while (paths.length > 0) {
+            const p = paths.shift();
+            if (p.length) obj = obj[p];
+        }
+        Object.assign(obj, mods[path]);
+    });
+    return definition;
+}
+
+function shouldHaveRejected() {
+    throw Error('Should have been rejected');
+}
