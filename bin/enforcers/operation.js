0de9a141c468bba8a176cfe851cc6ea8d3b18f81
james <james.speirs@gmail.com>
2018-10-11 16:23:30 -0600

    user getters instead of extend for parameters and parameters map

--- bin/enforcers/operation.js
+++ bin/enforcers/operation.js
@@ -22,24 +22,44 @@ const util          = require('../util');
 
 module.exports = Super(OperationEnforcer);
 
-OperationEnforcer.extend(function (data) {
+function OperationEnforcer(data) {
     const { definition, parent } = data;
 
-    // build the parameters map
-    if (parent) buildParametersMap(this.parametersMap, parent.parameters);
-    buildParametersMap(this.parametersMap, definition.parameters);
+    const parameters = [];
+    const parametersMap = {};
 
-    // overwrite the parameters array
-    Object.keys(this.parametersMap).forEach(at => {
-        Object.keys(this.parametersMap[at]).forEach(name => {
-            this.parameters.push(this.parametersMap[at][name]);
-        })
+    let parametersProcessed = false;
+
+    Object.defineProperties(this, {
+        parameters: {
+            get: function() {
+                let o;
+                if (!parametersProcessed) o = this.parametersMap;
+                return parameters;
+            }
+        },
+        parametersMap: {
+            get: function() {
+                if (!parametersProcessed) {
+
+                    // build the parameters map
+                    if (parent) buildParametersMap(parametersMap, parent.parameters);
+                    buildParametersMap(parametersMap, definition.parameters);
+
+                    // overwrite the parameters array
+                    Object.keys(parametersMap).forEach(at => {
+                        Object.keys(parametersMap[at]).forEach(name => {
+                            parameters.push(parametersMap[at][name]);
+                        })
+                    });
+
+                    parametersProcessed = true;
+                }
+                return parametersMap;
+            }
+        }
     });
-});
 
-function OperationEnforcer(data) {
-    this.parameters = [];
-    this.parametersMap = {};
 }
 
 
