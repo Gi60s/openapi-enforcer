34612ef3cd0aa91c343f76c15969c7dedd66499c
james <james.speirs@gmail.com>
2018-10-24 11:59:26 -0600

    fix super shared object error

--- bin/enforcers/super.js
+++ bin/enforcers/super.js
@@ -16,14 +16,21 @@
  **/
 'use strict';
 
-module.exports = function(Constructor, proto) {
+// 1. store individual enforcerData per instance using a map and a prototype getter
+// 2. store protected data per instance using a map. How?
+
+module.exports = function (Constructor) {
     const callbacks = [];
+    const store = new WeakMap();
 
-    Constructor.extend = function (callback) {
-        if (typeof callback !== 'function') throw Error('Invalid input. Callback must be a function. Received: ' + callback);
-        callbacks.push(callback);
-    };
+    // get the enforcer data for this instance
+    Object.defineProperty(Constructor.prototype, 'enforcerData', {
+        get: function () {
+            return store.get(this);
+        }
+    });
 
+    // define a method to turn the complex object into a plain object
     Constructor.prototype.toObject = function () {
         const result = {};
         Object.keys(this).forEach(key => {
@@ -45,29 +52,24 @@ module.exports = function(Constructor, proto) {
         return result;
     };
 
-    function build(f, data) {
-        const prototype = Object.create(f.prototype);
-        if (proto && typeof proto === 'object') Object.assign(prototype, proto);
-        prototype.enforcerData = data;
-        const context = Object.create(prototype);
-        Object.assign(context, data.definition);
-        Constructor.call(context, data);
+    // add extension to the class - these callbacks will execute as plugins when the entire tree has been built
+    Constructor.extend = function (callback) {
+        if (typeof callback !== 'function') throw Error('Invalid input. Callback must be a function. Received: ' + callback);
+        callbacks.push(callback);
+    };
 
-        // add callbacks to plugins
+    return function (data) {
+        const context = Object.create(Constructor.prototype);   // create the object
+        store.set(context, data);                               // store enforcer data
+        Object.assign(context, data.definition);                // copy definition properties to the object
+        Constructor.call(context, data);                        // call the constructor with context
+
+        // add callbacks to plugins - these will execute after the entire tree has been built
         const plugins = data.plugins;
         callbacks.forEach(callback => plugins.push(function() {
             callback.call(context, data);
         }));
 
         return context;
-    }
-
-    return new Function('build', 'Constructor',
-        `const f = function ${Constructor.name} (data) {
-            return build(f, data);
-        }
-        f.prototype = Object.create(Constructor.prototype)
-        f.constructor = f
-        return f`
-    )(build, Constructor);
+    };
 };
