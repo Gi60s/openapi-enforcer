113494dad6a95dccbe19d7adce6605364a9221e0
james <james.speirs@gmail.com>
2018-11-09 17:00:52 -0700

    group all operation tests into single file

--- test/enforcer.operation.test.js
+++ test/enforcer.operation.test.js
@@ -843,4 +843,513 @@ describe('enforcer/operation', () => {
 
     });
 
+    describe('definition', () => {
+        const responses = { default: { description: '' } };
+
+        describe('callbacks', () => {
+
+            it('is not allowed for v2', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    callbacks: {},
+                    responses
+                });
+                expect(err).to.match(/Property not allowed: callbacks/);
+            });
+
+            it('can be an object map of valid callback objects', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    callbacks: {
+                        eventX: {
+                            expression: {
+                                get: {
+                                    responses: {
+                                        default: {
+                                            description: 'ok'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an object map of valid callback objects', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    callbacks: {
+                        eventX: {
+                            expression: {
+                                get: {
+                                }
+                            }
+                        }
+                    },
+                    responses
+                });
+                expect(err).to.match(/Missing required property: responses/);
+            });
+
+        });
+
+        describe('consumes', () => {
+
+            it('is not valid for v3', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    consumes: [],
+                    responses
+                });
+                expect(err).to.match(/Property not allowed: consumes/);
+            });
+
+            it('can be an array of strings for v2', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    consumes: ['application/json'],
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an array', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    consumes: {},
+                    responses
+                });
+                expect(err).to.match(/Value must be an array/);
+            });
+
+            it('must be an array of strings', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    consumes: [1],
+                    responses
+                });
+                expect(err).to.match(/Value must be a string/);
+            });
+
+        });
+
+        describe('deprecated', () => {
+
+            it('can be a boolean', () => {
+                const [ def ] = Enforcer.v2_0.Operation({
+                    deprecated: true,
+                    responses
+                });
+                expect(def.deprecated).to.equal(true);
+            });
+
+            it('must be a boolean', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    deprecated: 1,
+                    responses
+                });
+                expect(err).to.match(/Value must be a boolean/);
+            });
+
+            it('defaults to false', () => {
+                const [ def ] = Enforcer.v2_0.Operation({
+                    responses
+                });
+                expect(def.deprecated).to.equal(false);
+            });
+
+        });
+
+        describe('description', () => {
+
+            it('can be a string', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    description: '',
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('cannot be a number', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    description: 1,
+                    responses
+                });
+                expect(err).to.match(/Value must be a string/);
+            });
+
+        });
+
+        describe('externalDocs', () => {
+
+            it('can be a valid external documentation object', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    externalDocs: {
+                        url: ''
+                    },
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be a valid external documentation object', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    externalDocs: {},
+                    responses
+                });
+                expect(err).to.match(/Missing required property: url/);
+            });
+
+        });
+
+        describe('operationId', () => {
+
+            it('can be a string', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    operationId: 'a',
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be a string', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    operationId: 1,
+                    responses
+                });
+                expect(err).to.match(/Value must be a string/);
+            });
+
+            it('must be unique among all operations', () => {
+                const [ , err ] = Enforcer.v2_0.PathItem({
+                    get: {
+                        operationId: 'a',
+                        responses
+                    },
+                    put: {
+                        operationId: 'a',
+                        responses
+                    }
+                });
+                expect(err).to.match(/The operationId must be unique/);
+            });
+
+        });
+
+        describe('parameters', () => {
+
+            it('can be an array of parameter objects', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    parameters: [
+                        { name: 'x', in: 'path', required: true, type: 'string' }
+                    ],
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an array of parameter objects', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    parameters: [{}],
+                    responses
+                });
+                expect(err).to.match(/Missing required properties: in, name, type/);
+            });
+
+            it('cannot have duplicate parameters', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    parameters: [
+                        { name: 'x', in: 'path', required: true, type: 'string' },
+                        { name: 'x', in: 'path', required: true, type: 'string' }
+                    ],
+                    responses
+                });
+                expect(err).to.match(/Parameter name must be unique per location/);
+            });
+
+            it('can only have one body parameter for v2', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    parameters: [
+                        { name: 'x', in: 'body', type: 'string' },
+                        { name: 'y', in: 'body', type: 'string' }
+                    ],
+                    responses
+                });
+                expect(err).to.match(/Only one body parameter allowed/);
+            });
+
+            it('cannot have both formData and body', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    parameters: [
+                        { name: 'x', in: 'body', type: 'string' },
+                        { name: 'y', in: 'formData', type: 'string' }
+                    ],
+                    responses
+                });
+                expect(err).to.match(/Cannot have parameters in "body" and "formData" simultaneously/);
+
+            })
+
+        });
+
+        describe('produces', () => {
+
+            it('is not valid for v3', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    produces: [],
+                    responses
+                });
+                expect(err).to.match(/Property not allowed: produces/);
+            });
+
+            it('can be an array of strings for v2', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    produces: ['application/json'],
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an array', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    produces: {},
+                    responses
+                });
+                expect(err).to.match(/Value must be an array/);
+            });
+
+            it('must be an array of strings', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    produces: [1],
+                    responses
+                });
+                expect(err).to.match(/Value must be a string/);
+            });
+
+        });
+
+        describe('requestBody', () => {
+
+            it('is not valid for v2', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    requestBody: {},
+                    responses
+                });
+                expect(err).to.match(/Property not allowed: requestBody/);
+            });
+
+            it('can be a valid request body object for v3', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    requestBody: {
+                        content: {
+                            'application/json': {
+                            }
+                        }
+                    },
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+        });
+
+        describe('responses', () => {
+
+            it('is required', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({});
+                expect(err).to.match(/Missing required property: responses/);
+            });
+
+            it('can be a valid response object', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            describe('codes', () => {
+
+                it('default ok', () => {
+                    const [ , err ] = Enforcer.v2_0.Operation({
+                        responses: {
+                            default: { description: '' }
+                        }
+                    });
+                    expect(err).to.be.undefined
+                });
+
+                it('200 ok', () => {
+                    const [ , err ] = Enforcer.v2_0.Operation({
+                        responses: {
+                            200: { description: '' }
+                        }
+                    });
+                    expect(err).to.be.undefined
+                });
+
+                it('2XX not ok for v2', () => {
+                    const [ , err ] = Enforcer.v2_0.Operation({
+                        responses: {
+                            '2XX': { description: '' }
+                        }
+                    });
+                    expect(err).to.match(/Property not allowed: 2XX/);
+                    expect(err.count).to.equal(1);
+                });
+
+                it('2XX ok for v3', () => {
+                    const [ , err ] = Enforcer.v3_0.Operation({
+                        responses: {
+                            '2XX': { description: '' }
+                        }
+                    });
+                    expect(err).to.be.undefined
+                });
+
+                it('600 not ok', () => {
+                    const [ , err ] = Enforcer.v2_0.Operation({
+                        responses: {
+                            '600': { description: '' }
+                        }
+                    });
+                    expect(err).to.match(/Property not allowed: 600/);
+                });
+
+            });
+
+        });
+
+        describe('schemes', () => {
+
+            it('is not allowed for v3', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    schemes: [],
+                    responses
+                });
+                expect(err).to.match(/Property not allowed: schemes/);
+            });
+
+            it('can be an array of valid strings', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    schemes: ['http'],
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an array of strings', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    schemes: [1],
+                    responses
+                });
+                expect(err).to.match(/Value must be a string/);
+            });
+
+            it('must match enum values', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    schemes: ['bob'],
+                    responses
+                });
+                expect(err).to.match(/Value must be one of: http, https, ws, wss/);
+            });
+
+        });
+
+        describe('security', () => {
+
+            it('can be an array', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    security: [],
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an array', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    security: {},
+                    responses
+                });
+                expect(err).to.match(/Value must be an array/);
+            });
+
+        });
+
+        describe('servers', () => {
+
+            it('is not allowed in v2', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    servers: [],
+                    responses
+                });
+                expect(err).to.match(/Property not allowed: servers/);
+            });
+
+            it('can be an array of valid server objects', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    servers: [{
+                        url: ''
+                    }],
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an array of valid server objects', () => {
+                const [ , err ] = Enforcer.v3_0.Operation({
+                    servers: [{}],
+                    responses
+                });
+                expect(err).to.match(/Missing required property: url/);
+            });
+
+        });
+
+        describe('summary', () => {
+
+            it('must be a string', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    summary: 1,
+                    responses
+                });
+                expect(err).to.match(/Value must be a string/);
+            });
+
+            it('should be less than 120 characters', () => {
+                const [ , err, warning ] = Enforcer.v2_0.Operation({
+                    summary: '123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 ',
+                    responses
+                });
+                expect(err).to.be.undefined;
+                expect(warning).to.match(/Value should be less than 120 characters/);
+            });
+
+        });
+
+        describe('tags', () => {
+
+            it('allows an array of strings', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    tags: ['a', 'b'],
+                    responses
+                });
+                expect(err).to.be.undefined;
+            });
+
+            it('must be an array', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    tags: 1,
+                    responses
+                });
+                expect(err).to.match(/Value must be an array/);
+            });
+
+            it('must be an array of strings', () => {
+                const [ , err ] = Enforcer.v2_0.Operation({
+                    tags: [1],
+                    responses
+                });
+                expect(err).to.match(/Value must be a string/);
+            });
+
+        });
+
+    });
+
 });
