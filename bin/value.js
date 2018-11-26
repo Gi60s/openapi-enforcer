9e52ca11ecbd05f653b9882a4f58e4b0d8246cc9
james <james.speirs@gmail.com>
2018-11-26 14:40:33 -0700

    remove coerce, add populate

--- bin/value.js
+++ bin/value.js
@@ -18,7 +18,7 @@
 
 module.exports = EnforcerValue;
 
-const defaultCoerce = false;
+const defaultPopulate = true;
 const defaultSerialize = true;
 const defaultValidate = true;
 
@@ -27,7 +27,7 @@ const defaultValidate = true;
  * will be created with handling overwritten where specified.
  * @param {*} value
  * @param {object} [config]
- * @param {boolean} [config.coerce] Whether the value should use strong type coercion.
+ * @param {boolean} [config.populate] Whether the value should be populated.
  * @param {boolean} [config.serialize] Whether the value should be serialized or deserialized.
  * @param {boolean} [config.validate] Whether the value should be validated.
  * @returns {EnforcerValue}
@@ -38,15 +38,15 @@ function EnforcerValue(value, config) {
 
     if (typeof value === 'object' && value instanceof EnforcerValue) {
         config = {
-            coerce: config.coerce === undefined ? value.coerce : config.coerce,
+            populate: config.populate === undefined ? value.populate : config.populate,
             serialize: config.serialize === undefined ? value.serialize : config.serialize,
             validate: config.validate === undefined ? value.validate : config.validate
         };
         value = value.value;
     }
 
-    const { coerce, serialize, validate } = config;
-    this.coerce = coerce;
+    const { populate, serialize, validate } = config;
+    this.populate = populate;
     this.serialize = serialize;
     this.validate = validate;
     this.value = value;
@@ -54,11 +54,11 @@ function EnforcerValue(value, config) {
 
 /**
  * Get the attributes (with defaults applied) for an enforcer value.
- * @returns {{coerce: boolean, serialize: boolean, validate: boolean, value: *}}
+ * @returns {{populate: boolean, serialize: boolean, validate: boolean, value: *}}
  */
 EnforcerValue.prototype.attributes = function () {
     return {
-        coerce: this.coerce === undefined ? defaultCoerce : this.coerce,
+        populate: this.populate === undefined ? defaultPopulate : this.populate,
         serialize: this.serialize === undefined ? defaultSerialize : this.serialize,
         validate: this.validate === undefined ? defaultValidate : this.validate,
         value: this.value
@@ -66,28 +66,19 @@ EnforcerValue.prototype.attributes = function () {
 };
 
 /**
- * Shorthand method for creating an EnforcerValue instance with coerce set to true.
- * @param {*} value
- * @returns {EnforcerValue}
- */
-EnforcerValue.coerce = function (value) {
-    return new EnforcerValue(value, { coerce: true });
-};
-
-/**
  * Create a new EnforcerValue instance that keeps it's own configuration over a supplied secondary configuration.
  * @param {*} value
  * @param {object} [config]
- * @param {boolean} [config.coerce] Whether the value should use strong type coercion.
+ * @param {boolean} [config.populate] Whether the value should use strong type coercion.
  * @param {boolean} [config.serialize] Whether the value should be serialized or deserialized.
  * @param {boolean} [config.validate] Whether the value should be validated.
  * @returns {EnforcerValue}
  */
 EnforcerValue.inherit = function (value, config) {
     if (typeof value === 'object' && value instanceof EnforcerValue) {
-        const { coerce, serialize, validate } = value;
+        const { populate, serialize, validate } = value;
         const result = new EnforcerValue(value.value);
-        if (coerce === undefined && config.coerce !== undefined) result.coerce = config.coerce;
+        if (populate === undefined && config.populate !== undefined) result.populate = config.populate;
         if (serialize === undefined && config.serialize !== undefined) result.serialize = config.serialize;
         if (validate === undefined && config.validate !== undefined) result.validate = config.validate;
         return result;
@@ -99,13 +90,13 @@ EnforcerValue.inherit = function (value, config) {
 /**
  * Get EnforcerValue attributes whether the value is actually an EnforcerValue instance or a plain value.
  * @param {*} value
- * @returns {{coerce: boolean, serialize: boolean, validate: boolean, value: *}}
+ * @returns {{populate: boolean, serialize: boolean, validate: boolean, value: *}}
  */
 EnforcerValue.getAttributes = function (value) {
     return typeof value === 'object' && value instanceof EnforcerValue
         ? value.attributes()
         : {
-            coerce: defaultCoerce,
+            populate: defaultPopulate,
             serialize: defaultSerialize,
             validate: defaultValidate,
             value
