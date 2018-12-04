3f2b05e3273cb24fa6f26d553f53c9e23e5b8b7d
james <james.speirs@gmail.com>
2018-12-03 17:46:37 -0700

    working

--- bin/schema/random.js
+++ bin/schema/random.js
@@ -21,143 +21,160 @@ const { copy, randomOneOf: chooseOne, randomNumber, randomText } = util;
 
 module.exports = runRandom;
 
-function runRandom(exception, schema, value, options, depth) {
-    if (!options) options = {};
-    const { arrayVariation = 4, defaultPossibility = .25, numberVariation = 1000, } = options;
+function runRandom(exception, warn, map, schema, parent, property, options, depth) {
     const type = schema.type;
+    const value = parent[property];
 
-    // 15% chance of using default value
-    if (schema.hasOwnProperty('default') && Math.random() < defaultPossibility) return schema.default;
+    if (value && typeof value === 'object') {
+        const existing = map.get(value);
+        if (existing) return;
+        map.set(value, true)
+    }
 
-    // select an enum value
-    if (schema.enum) return copy(chooseOne(schema.enum));
+    if (depth > options.maxDepth) return;
 
-    if (schema.allOf) {
-        exception.message('Cannot generate random value for schema with allOf');
+    if (schema.hasOwnProperty('default') && Math.random() < options.defaultPossibility) {
+        parent[property] = util.copy(schema.default);
+
+    } else if (schema.enum) {
+        parent[property] = copy(chooseOne(schema.enum));
+
+    } else if (schema.allOf) {
+        warn.message('Cannot generate random value for schema with allOf');
 
     } else if (schema.anyOf || schema.oneOf) {
         const mode = schema.anyOf ? 'anyOf' : 'oneOf';
-        const subSchema = schema.discriminate(value) || chooseOne(schema[mode]);
-        return runRandom(exception, subSchema, value, options, depth);
+        const subSchema = schema.discriminator ? schema.discriminate(value) : chooseOne(schema[mode]);
+        runRandom(exception, warn, map, subSchema, parent, property, options, depth);
 
     } else if (schema.not) {
-        exception.message('Cannot generate random value for schema with not');
+        warn.message('Cannot generate random value for schema with not');
 
     } else if (type === 'array') {
         const min = schema.hasOwnProperty('minItems') ? schema.minItems : 0;
-        let max = schema.hasOwnProperty('maxItems') ? schema.maxItems : min + arrayVariation - Math.round(.5 * depth);
+        let max = schema.hasOwnProperty('maxItems') ? schema.maxItems : min + options.arrayVariation - Math.round(.5 * depth);
         if (max < min) max = min;
         const length = randomNumber({ min, max });
-        const array = Array.isArray(value) ? value.concat() : [];
-        let duplicates = 0;
-        let index;
-        while ((index = array.length) < length) {
-            if (array[index] === undefined) {
-                const value = runRandom(exception.at(index), schema.items, array[index], options, depth + 1);
-                if (schema.uniqueItems) {
-                    const match = array.find(v => util.same(v, value));
-                    if (match) {
-                        duplicates++;
-                        if (duplicates > 5) exception.message('Cannot generate example due to too narrowly scoped schema constraints');
-                    } else {
-                        duplicates = 0;
+        if (depth > 0 && parent.hasOwnProperty(property) && !Array.isArray(parent[property])) {
+            exception.message('Provided value is not an array');
+        } else {
+            const array = Array.isArray(value) ? value : [];
+            for (let index = 0; index < length; index++) {
+                let retry = true;
+                let retriesRemaining = options.uniqueItemRetry;
+                while (retry && retriesRemaining) {
+                    const o = {};
+                    runRandom(exception.at(index), warn.at(index), map, schema.items, o, 'value', options, depth + 1);
+                    const value = o.value;
+                    retry = schema.uniqueItems && array.findIndex((v) => util.same(v, value)) !== -1;
+                    if (!retry) {
                         array[index] = value;
+                    } else if (retry && !retriesRemaining) {
+                        warn.message('Cannot generate example due to too narrowly scoped schema constraints');
                     }
-                } else {
-                    array[index] = value;
                 }
             }
+            parent[property] = array;
         }
-        return array;
 
     } else if (type === 'object') {
-        const result = Object.assign({}, value);
-        const max = schema.hasOwnProperty('maxProperties') ? schema.maxProperties : Number.MAX_SAFE_INTEGER;
-        let remaining = max - Object.keys(result).length;
-        if (schema.properties) {
-
-            // separate properties by optional or required
-            const optionalKeys = [];
-            const requiredKeys = [];
-            Object.keys(schema.properties).forEach(key => {
-                schema.properties[key].required ? requiredKeys.push(key) : optionalKeys.push(key);
-            });
-
-            // populate required properties
-            requiredKeys.forEach(key => {
-                if (result[key] === undefined) {
-                    result[key] = runRandom(exception.at(key), schema.properties[key], undefined, options, depth + 1);
-                    remaining--;
-                }
-            });
-
-            // populate optional properties until max properties reached
-            let length = optionalKeys.length;
-            while (remaining && length) {
-                const index = Math.floor(Math.random() * length);
-                const key = optionalKeys.splice(index, 1)[0];
-                length--;
-                if (result[key] === undefined) {
-                    remaining--;
-                    result[key] = runRandom(exception.at(key), schema.properties[key], undefined, options, depth + 1);
-                }
-            }
-        }
-
-        if (schema.additionalProperties && remaining) {
+        if (depth > 0 && parent.hasOwnProperty(property) && !util.isPlainObject(parent[property])) {
+            exception.message('Provided value is not a plain object');
+        } else {
+            const definedProperties = schema.properties ? Object.keys(schema.properties) : [];
+            const object = parent[property] || {};
             let count = 0;
-            let index = 1;
-            while (count < 3 && remaining) {
-                const key = 'additionalProperty' + index++;
-                if (!result.hasOwnProperty(key)) {
+
+            // add required properties first
+            if (schema.required) {
+                schema.required.forEach(key => {
+                    const index = definedProperties.indexOf(key);
+                    if (index !== -1) definedProperties.splice(index, 1);
+
+                    let subSchema = schema.properties && schema.properties[key];
+                    if (!subSchema) subSchema = schema.additionalProperties;
+                    if (subSchema === true) subSchema = randomSchema(schema);
+                    runRandom(exception.at(key), warn.at(key), map, subSchema, object, key, options, depth + 1);
+
                     count++;
-                    remaining--;
-                    result[key] = runRandom(exception.at(key), schema.additionalProperties === true ? randomSchema() : schema.additionalProperties, undefined, options, depth + 1);
+                });
+            }
+
+            // add defined properties (in random order)
+            let definedPropertiesLength = definedProperties.length;
+            const maxProperties = schema.hasOwnProperty('maxProperties') ? schema.maxProperties : Number.MAX_SAFE_INTEGER;
+            while (definedPropertiesLength && count < maxProperties) {
+                if (Math.random() < options.definedPropertyPossibility) {
+                    const index = Math.floor(Math.random() * definedPropertiesLength);
+                    const key = definedProperties[index];
+                    runRandom(exception.at(key), warn.at(key), map, schema.properties[key], object, key, options, depth + 1);
+                    count++;
+                }
+                definedPropertiesLength--;
+            }
+
+            // add additional properties
+            if (schema.additionalProperties) {
+                const minProperties = schema.hasOwnProperty('minProperties') ? schema.minProperties : 0;
+                let addMoreProperties = count < minProperties || Math.random() < options.additionalPropertiesPossibility;
+                let additionalPropertiesIndex = 1;
+                while (addMoreProperties && count < maxProperties) {
+                    const key = 'additionalProperty' + additionalPropertiesIndex++;
+                    if (!object.hasOwnProperty(key)) {
+                        const subSchema = schema.additionalProperties === true ? randomSchema(schema) : schema.additionalProperties;
+                        runRandom(exception.at(key), warn.at(key), map, subSchema, object, key, options, depth + 1);
+                        count++;
+                        addMoreProperties = count < minProperties || Math.random() < options.additionalPropertiesPossibility;
+                    }
                 }
             }
+
+            parent[property] = object;
         }
 
-        return result;
-
-    } else if (value === undefined) {
+    } else if (!parent.hasOwnProperty(property) || depth === 0) {
         const dataTypes = schema.enforcerData.staticData.dataTypes;
         const dataType = dataTypes[schema.type][schema.format] || null;
 
         if (dataType) {
-            return dataType.random(schema, { chooseOne, randomNumber, randomText });
+            parent[property] = dataType.random(schema, { chooseOne, randomNumber, randomText });
 
         } else if (type === 'boolean') {
-            return chooseOne([true, false]);
+            parent[property] = chooseOne([true, false]);
 
         } else if (type === 'integer' || type === 'number') {
             const decimalPlaces = type === 'integer' ? 0 : randomNumber({min: 0, max: 4});
             const exclusiveMin = !!schema.exclusiveMinimum;
             const exclusiveMax = !!schema.exclusiveMaximum;
-            const min = schema.hasOwnProperty('minimum') ? schema.minimum : -1 * Math.floor(numberVariation * .25);
-            const max = schema.hasOwnProperty('maximum') ? schema.maximum : Math.ceil(numberVariation * .75);
-            return randomNumber({min, max, exclusiveMin, exclusiveMax, decimalPlaces});
+            const multipleOf = schema.hasOwnProperty('multipleOf') ? schema.multipleOf : 0;
+            const min = schema.hasOwnProperty('minimum') ? schema.minimum : -1 * Math.floor(options.numberVariation * .25);
+            const max = schema.hasOwnProperty('maximum') ? schema.maximum : Math.ceil(options.numberVariation * .75);
+            parent[property] = randomNumber({min, max, multipleOf, exclusiveMin, exclusiveMax, decimalPlaces});
 
         } else if (type === 'string') {
-            const options = {};
-            if (schema.hasOwnProperty('minLength')) options.minLength = schema.minLength;
-            if (schema.hasOwnProperty('maxLength')) options.maxLength = schema.maxLength;
-            return randomText(options)
-
+            if (schema.hasOwnProperty('pattern')) {
+                warn.message('Cannot generate random value that matches a pattern');
+            } else {
+                const options = {};
+                if (schema.hasOwnProperty('minLength')) options.minLength = schema.minLength;
+                if (schema.hasOwnProperty('maxLength')) options.maxLength = schema.maxLength;
+                parent[property] = randomText(options)
+            }
         }
-    } else {
-        return value;
     }
 }
 
-function randomSchema() {
-    switch (Math.floor(Math.random() * 8)) {
-        case 0: return { type: 'boolean' };
-        case 1: return { type: 'integer' };
-        case 2: return { type: 'number' };
-        case 3: return { type: 'string' };
-        case 4: return { type: 'string', format: 'binary' };
-        case 5: return { type: 'string', format: 'byte' };
-        case 6: return { type: 'string', format: 'date' };
-        case 7: return { type: 'string', format: 'date-time' };
-    }
+function randomSchema(ref) {
+    // type "string" format "binary" or "byte" cannot be serialized or deserialized w/o schema
+    // so they are omitted from the list of possible random schemas
+    const def = chooseOne([
+        { type: 'boolean' },
+        { type: 'integer' },
+        { type: 'number' },
+        { type: 'string' },
+        { type: 'string', format: 'date' },
+        { type: 'string', format: 'date-time' }
+    ]);
+    const [ schema ] = new ref.enforcerData.context.Schema(def);
+    return schema;
 }
