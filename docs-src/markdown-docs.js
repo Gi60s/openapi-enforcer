27fbf79aaea66c4d125ff3c3adc913c90242f654
james <james.speirs@gmail.com>
2019-05-22 14:38:50 -0600

    add a general description

--- docs-src/markdown-docs.js
+++ docs-src/markdown-docs.js
@@ -6,6 +6,7 @@ module.exports = {
   site: {
     editSourceUrl: 'https://github.com/byu-oit/openapi-enforcer/tree/master/docs-src',
     title: 'Open API Enforcer',
+    description: 'A library that makes it easy to validate and use your Open API documents.',
     url: 'https://byu-oit.github.io/openapi-enforcer',
   },
   template: {
