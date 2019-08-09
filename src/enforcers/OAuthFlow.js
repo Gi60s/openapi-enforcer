4e3851c7b4d4ff72df8ae2213a0bcfdab5aecb84
james <james.speirs@gmail.com>
2019-08-09 10:24:12 -0600

    refresh url not required

--- src/enforcers/OAuthFlow.js
+++ src/enforcers/OAuthFlow.js
@@ -34,7 +34,6 @@ module.exports = {
                     type: 'string'
                 },
                 refreshUrl: {
-                    required: true,
                     type: 'string'
                 },
                 scopes: {
