977d090194e052d51a42c8b1c73e2bd991d2baba
james <james.speirs@gmail.com>
2019-12-02 11:35:34 -0700

    reworking

--- src/json-schema-ref-parser.js
+++ src/json-schema-ref-parser.js
@@ -16,10 +16,12 @@
  **/
 'use strict';
 
+const Exception = require('./exception');
 const fs        = require('fs');
 const http      = require('http');
 const https     = require('https');
 const path      = require('path');
+const Result    = require('./result');
 const url       = require('url');
 const yaml      = require('js-yaml');
 
@@ -27,19 +29,74 @@ const rxHttp = /^https?:\/\//i;
 const rxYaml = /\.ya?ml$/i;
 const rxJson = /\.json$/i;
 
-module.exports = RefParser;
+module.exports = dereference;
 
-async function RefParser (source, exception, warning) {
+/**
+ * @typedef ParserData
+ * @property {EnforcerException} exception The exception object at the current node.
+ * @property {string|number|undefined} key The key of the parent that arrived to this node.
+ * @property {object} loads A map of HTTP or file paths that have been loaded.
+ * @property {WeakMap} map A map of references, used to handle circular referencing.
+ * @property {ParserData} parent The parent parse data object.
+ * @property {string} path The HTTP or file path to the root node for this node.
+ * @property {string|object} source The source object or string.
+ * @property {object} result The result object.
+ * @property {ParserData} root The root parser data object.
+ * @property {EnforcerException} warning The exception object at the current node.
+ */
+
+/**
+ * Dereference a source.
+ * @param {string|object} source
+ * @returns {Promise<EnforcerResult>}
+ */
+
+async function dereference (source) {
+    const data = {
+        exception: new Exception('Unable to dereference definition for one or more reasons'),
+        key: undefined,
+        loads: Object.create(null),
+        map: new WeakMap(),
+        parent: null,
+        path: typeof source === 'string' ? source : process.cwd(),
+        source: source,
+        result: null,
+        warning: new Exception('One or more warnings encountered while while dereferencing definition')
+    };
+    data.root = data;
+
+    // if the source is a string the load the content
+    if (typeof source === 'string') {
+        const [ refPath, ref ] = source.split('#/');
+        const obj = await load(refPath, data);
+        data.source = obj
+            ? traverse(obj, ref, data.exception)
+            : null;
+    }
+
+    if (data.source) await parse(data);
+
+    if (data.exception.hasException) {
+        return Result(null, data.exception, data.warning);
+    } else {
+        const factory = {
+
+            value: data.result
+        };
+        return Result(factory, data.exception, data.warning);
+    }
+}
+
+RefParser.prototype.dereference = async function (source) {
+    const exception = new Exception('Unable to dereference definition for one or more reasons');
+    const warning = new Exception('One or more warnings encountered while while dereferencing definition');
+
     const data = {
         exception: exception,
         key: undefined,
         path: typeof source === 'string' ? source : process.cwd(),
         parent: null,
-        shared: {
-            circular: false,
-            map: new WeakMap(),
-            refs: Object.create(null)
-        },
+        shared: this.$refs,
         warning: warning
     };
 
@@ -53,8 +110,10 @@ async function RefParser (source, exception, warning) {
         }
     }
 
-    return parse(obj.value, data);
-}
+    const value = await parse(obj && obj.value, data);
+    this.$refs.root = value;
+    return Result(value, exception, warning);
+};
 
 function childData (data, key, path) {
     return {
@@ -62,6 +121,7 @@ function childData (data, key, path) {
         key: key,
         path: path || data.path,
         parent: data,
+        root: data.root,
         shared: data.shared,
         warning: data.warning.at(key)
     }
@@ -96,10 +156,17 @@ async function parse (obj, data) {
         if (existing) return existing;
 
         if (obj.hasOwnProperty('$ref')) {
-            const loaded = await load(data.path, obj.$ref, data.exception, data.shared.refs);
+            let loaded;
+            console.log('$ref: ' + data.path + ' ' + obj.$ref);
+            if (obj.$ref.startsWith('#/')) {
+                loaded = await load(data.path, obj.$ref, data.exception, data.shared.refs);
+            } else {
+
+            }
             if (loaded) {
+                console.log('Loaded: ' + data.path);
                 const [, internalPath] = obj.$ref.split('#');
-                const value = traverse(loaded.value, internalPath, data.exception);
+                const value = traverse(loaded.value, internalPath, data.exception.at(loaded.path));
                 return parse(value, Object.assign({}, data, { path: loaded.path }))
             }
         } else {
@@ -119,75 +186,55 @@ async function parse (obj, data) {
 
 /**
  * Load a file or URL and parse to object.
- * @param {string} basePath
- * @param {string} ref
- * @param {EnforcerException} exception
- * @param {object} refs
+ * @param {string} resourcePath
+ * @param {ParserData} data
  * @returns {Promise<{path: string, value: *}|undefined>}
  */
-async function load (basePath, ref, exception, refs) {
-    const [refPath] = ref.split('#');
-
-    // determine load path and method
-    let loadPath;
-    let loadMethod;
-    if (rxHttp.test(basePath) || rxHttp.test(ref)) {
-        loadPath = url.resolve(basePath, refPath);
-        loadMethod = httpLoad;
-    } else if (refPath) {
-        loadPath = path.resolve(path.dirname(basePath), refPath);
-        loadMethod = fileLoad;
-    } else {
-        loadPath = path.resolve(basePath);
-        loadMethod = fileLoad;
-    }
-
+async function load (resourcePath, { exception, loads }) {
     // if already loaded or loading then return that promise
-    if (refs[loadPath]) {
-        return refs[loadPath];
-    }
+    if (loads[resourcePath]) return loads[resourcePath];
 
     // store deferred promise
     const deferred = defer();
-    refs[loadPath] = deferred.promise;
+    loads[resourcePath] = deferred.promise;
 
     // load content
-    const data = await loadMethod(loadPath, exception);
+    const loadMethod = rxHttp.test(resourcePath) ? httpLoad : fileLoad;
+    const data = await loadMethod(resourcePath, exception);
 
     // determine content type
     let type = data ? data.type : undefined;
-    if (!type && rxYaml.test(loadPath)) type = 'yaml';
-    if (!type && rxJson.test(loadPath)) type = 'json';
+    if (!type && rxYaml.test(resourcePath)) type = 'yaml';
+    if (!type && rxJson.test(resourcePath)) type = 'json';
 
-    let obj;
-    if (data) {
-        if (type === 'yaml') {
-            obj = yaml.safeLoad(data.content);
-        } else if (type === 'json') {
-            obj = JSON.parse(data.content);
-        } else if (!type) {
-            try {
-                obj = yaml.safeLoad(data.content);
-                type = 'yaml';
-            } catch (err) {}
-            if (!type) {
+    try {
+        if (data) {
+            if (type === 'yaml') {
+                deferred.resolve(yaml.safeLoad(data.content));
+            } else if (type === 'json') {
+                deferred.resolve(JSON.parse(data.content));
+            } else if (!type) {
+                let obj;
                 try {
-                    obj = JSON.parse(data.content);
-                    type = 'json';
+                    obj = yaml.safeLoad(data.content);
+                    type = 'yaml';
                 } catch (err) {}
-            }
-            if (!type) {
-                exception.message('Unable to parse document "' + refPath + '". Please verify that it is either a YAML or JSON document.');
+                if (!type) {
+                    try {
+                        obj = JSON.parse(data.content);
+                        type = 'json';
+                    } catch (err) {}
+                }
+                if (!type) {
+                    exception.message('Unable to parse document "' + resourcePath + '". Please verify that it is either a YAML or JSON document.');
+                    deferred.resolve();
+                } else {
+                    deferred.resolve(obj);
+                }
             }
         }
-    }
-
-    if (!exception.hasException) {
-        deferred.resolve({
-            path: loadPath,
-            value: obj
-        });
-    } else {
+    } catch (err) {
+        exception.message(err.toString());
         deferred.resolve();
     }
 
@@ -260,6 +307,26 @@ function fileLoad (filePath, exception) {
     })
 }
 
+function resolvePath (basePath, ref) {
+    const [refPath] = ref.split('#');
+    if (rxHttp.test(basePath) || rxHttp.test(ref)) {
+        return {
+            loadPath: url.resolve(basePath, refPath),
+            loadMethod: httpLoad
+        };
+    } else if (refPath) {
+        return {
+            loadPath: path.resolve(path.dirname(basePath), refPath),
+            loadMethod: fileLoad
+        };
+    } else {
+        return {
+            loadPath: path.resolve(basePath),
+            loadMethod: fileLoad
+        };
+    }
+}
+
 function traverse (obj, path, exception) {
     if (!path) return obj;
 
@@ -276,7 +343,7 @@ function traverse (obj, path, exception) {
             if (o.hasOwnProperty(key)) {
                 o = o[key];
             } else {
-                exception.message('Cannot resolve reference: ' + path);
+                exception.message('Cannot resolve reference: #' + path);
                 return;
             }
         }
