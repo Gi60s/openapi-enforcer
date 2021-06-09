d904cccf83eb653219544762e3bb2dd5204fe4e2
James Speirs <james.speirs@gmail.com>
2021-06-08 22:27:38 -0600

    working on dereference tests

--- src/test/discriminator.test.ts
+++ src/test/discriminator.test.ts
@@ -1,5 +1,8 @@
+import { OpenAPI } from '../components/OpenAPI'
 import { Discriminator } from '../components/Discriminator'
 import { expect } from 'chai'
+import path from 'path'
+import { resourcesDirectory, server } from '../test-utils'
 
 describe.only('Discriminator component', () => {
   describe('build', () => {
@@ -58,13 +61,15 @@ describe.only('Discriminator component', () => {
         expect(error).to.match(/Expected a non-null object/)
       })
 
-      it.only('will warn of unresolvable mappings', function () {
-        const [, warn] = Discriminator.validate({
-          propertyName: 'petType',
-          mapping: {
-            dog: '#/components/schemas/Dog'
-          }
-        })
+      it.only('will warn of unresolvable mappings', async function () {
+        const filePath = path.resolve(resourcesDirectory, 'discriminator', 'one-of.yml')
+        const [openapi, error, warn] = await OpenAPI.load(filePath)
+        // const [, warn] = Discriminator.validate({
+        //   propertyName: 'petType',
+        //   mapping: {
+        //     dog: '#/components/schemas/Dog'
+        //   }
+        // })
         expect(warn).to.match(/Expected a non-null object/)
       })
 
