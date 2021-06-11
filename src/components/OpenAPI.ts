628eacf0ac32e9811d6dbb161c43a697a895ccce
James Speirs <james.speirs@gmail.com>
2021-06-10 23:57:34 -0600

    add load functionality to openapi component

--- src/components/OpenAPI.ts
+++ src/components/OpenAPI.ts
@@ -1,4 +1,4 @@
-import { OASComponent, initializeData, LoaderOptions, rootComponentLoader, SchemaObject, SpecMap, Version, Exception } from './'
+import { OASComponent, initializeData, LoaderOptions, normalizeLoaderOptions, SchemaObject, SpecMap, Version, Exception } from './'
 import * as Loader from '../loader'
 import { addExceptionLocation, yes } from '../util'
 import * as E from '../Exception/methods'
@@ -9,7 +9,7 @@ import * as Paths from './Paths'
 import * as SecurityRequirement from './SecurityRequirement'
 import * as Server from './Server'
 import * as Tag from './Tag'
-import { lookup } from '../loader'
+import { LoaderMetadata, lookupLocation } from '../loader'
 import { Result } from '../Result'
 
 const rxVersion = /^\d+\.\d+\.\d+$/
@@ -51,8 +51,37 @@ export class OpenAPI extends OASComponent {
     }
   }
 
-  static async load (path: string, options?: LoaderOptions): Promise<Result<Definition>> {
-    return await rootComponentLoader(path, this, options ?? {})
+  static async load (path: string, options?: LoaderOptions): Promise<Result<OpenAPI>> {
+    options = normalizeLoaderOptions(options)
+
+    // load file with dereference
+    const config: LoaderMetadata = {
+      cache: {},
+      exception: new Exception('Unable to load OpenAPI document')
+    }
+    const loaded = await Loader.load(path, { dereference: options.dereference }, config)
+
+    // if there is an error then return now
+    const [definition] = loaded
+    const exception = loaded.exception as Exception
+    if (loaded.hasError) return new Result(definition, exception) // first param will be undefined because of error
+
+    // initialize data object
+    const version = definition.openapi
+    const data = initializeData('', definition, version)
+    data.loadCache = config.cache as Record<string, any>
+    data.exception = exception
+
+    // run validation then reset the map
+    // @ts-expect-error
+    if (options.validate === true) OpenAPI.validate(definition, version, data)
+    data.map = new Map()
+
+    // build the component if there are no errors
+    if (exception.hasError) return new Result(definition, exception) // first param will be undefined because of error
+    // @ts-expect-error
+    const component = new OpenAPI(definition, version, data)
+    return new Result(component, exception)
   }
 
   static schemaGenerator (): SchemaObject {
@@ -93,13 +122,13 @@ export class OpenAPI extends OASComponent {
             before: ({ definition, exception, reference }, component) => {
               if (!rxVersion.test(definition)) {
                 const invalidSemanticVersionNumber = E.invalidSemanticVersionNumber(reference)
-                addExceptionLocation(invalidSemanticVersionNumber, lookup(component, 'openapi', 'value'))
+                addExceptionLocation(invalidSemanticVersionNumber, lookupLocation(component, 'openapi', 'value'))
                 exception.message(invalidSemanticVersionNumber)
                 return false
               }
               if (definition.split('.')[0] !== '3') {
                 const invalidOpenApiVersionNumber = E.invalidOpenApiVersionNumber(reference, definition)
-                addExceptionLocation(invalidOpenApiVersionNumber, lookup(component, 'openapi', 'value'))
+                addExceptionLocation(invalidOpenApiVersionNumber, lookupLocation(component, 'openapi', 'value'))
                 exception.message(invalidOpenApiVersionNumber)
                 return false
               }
---
+++ src/test/openapi.test.ts
@@ -0,0 +1,22 @@
+import { OpenAPI } from '../components/OpenAPI'
+import { Reference } from '../components/Reference'
+import { Schema } from '../components/Schema'
+import { expect } from 'chai'
+import path from 'path'
+import { resourcesDirectory } from '../test-utils'
+
+describe('OpenAPI component', function () {
+  describe.only('load', () => {
+    it('can load and dereference', async () => {
+      const filePath = path.resolve(resourcesDirectory, 'discriminator', 'one-of.yml')
+      const [openapi] = await OpenAPI.load(filePath)
+      expect(openapi?.components?.schemas?.Pet.oneOf[0]).to.be.instanceof(Schema)
+    })
+
+    it('can load without dereference', async () => {
+      const filePath = path.resolve(resourcesDirectory, 'discriminator', 'one-of.yml')
+      const [openapi] = await OpenAPI.load(filePath, { dereference: false })
+      expect(openapi?.components?.schemas?.Pet.oneOf[0]).to.be.instanceof(Reference)
+    })
+  })
+})
