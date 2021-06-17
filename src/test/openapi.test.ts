9baa4ac7ce9f65d8c284ea4ff50b6871f129366f
James Speirs <james.speirs@gmail.com>
2021-06-17 10:36:37 -0600

    add loading tests

--- src/test/openapi.test.ts
+++ src/test/openapi.test.ts
@@ -11,12 +11,18 @@ describe('OpenAPI component', function () {
       const filePath = path.resolve(resourcesDirectory, 'discriminator', 'one-of.yml')
       const [openapi] = await OpenAPI.load(filePath)
       expect(openapi?.components?.schemas?.Pet.oneOf[0]).to.be.instanceof(Schema)
+      expect(openapi?.components?.schemas?.Pet.oneOf[1]).to.be.instanceof(Schema)
+      expect(openapi?.components?.schemas?.Pet.oneOf[2]).to.be.instanceof(Schema)
+      expect(openapi?.components?.schemas?.Pet.oneOf[3]).to.be.instanceof(Schema)
     })
 
     it('can load without dereference', async () => {
       const filePath = path.resolve(resourcesDirectory, 'discriminator', 'one-of.yml')
       const [openapi, error] = await OpenAPI.load(filePath, { dereference: false })
       expect(openapi?.components?.schemas?.Pet.oneOf[0]).to.be.instanceof(Reference)
+      expect(openapi?.components?.schemas?.Pet.oneOf[1]).to.be.instanceof(Reference)
+      expect(openapi?.components?.schemas?.Pet.oneOf[2]).to.be.instanceof(Reference)
+      expect(openapi?.components?.schemas?.Pet.oneOf[3]).to.be.instanceof(Reference)
     })
   })
 })
