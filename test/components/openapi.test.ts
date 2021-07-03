7ae04c604cc7727b223e7136a14e34ca02722df5
James Speirs <james.speirs@gmail.com>
2021-07-03 15:14:21 -0600

    move tests to discriminator because that is what was actually being tested

--- test/components/openapi.test.ts
+++ test/components/openapi.test.ts
@@ -7,23 +7,6 @@ import { resourcesDirectory } from '../util/helpers'
 
 describe('OpenAPI component', function () {
   describe('load', () => {
-    it('can load and dereference', async () => {
-      const filePath = path.resolve(resourcesDirectory, 'discriminator', 'one-of.yml')
-      const result = await OpenAPI.load(filePath)
-      const [openapi] = result
-      expect(openapi?.components?.schemas?.Pet.oneOf[0]).to.be.instanceof(Schema)
-      expect(openapi?.components?.schemas?.Pet.oneOf[1]).to.be.instanceof(Schema)
-      expect(openapi?.components?.schemas?.Pet.oneOf[2]).to.be.instanceof(Schema)
-      expect(openapi?.components?.schemas?.Pet.oneOf[3]).to.be.instanceof(Schema)
-    })
 
-    it('can load without dereference', async () => {
-      const filePath = path.resolve(resourcesDirectory, 'discriminator', 'one-of.yml')
-      const [openapi] = await OpenAPI.load(filePath, { dereference: false })
-      expect(openapi?.components?.schemas?.Pet.oneOf[0]).to.be.instanceof(Reference)
-      expect(openapi?.components?.schemas?.Pet.oneOf[1]).to.be.instanceof(Reference)
-      expect(openapi?.components?.schemas?.Pet.oneOf[2]).to.be.instanceof(Reference)
-      expect(openapi?.components?.schemas?.Pet.oneOf[3]).to.be.instanceof(Reference)
-    })
   })
 })
