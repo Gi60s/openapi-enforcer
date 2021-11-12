f27adce1877dfa2f8b036bc5ad2828d34a87dc9d
James Speirs <james.speirs@gmail.com>
2021-11-11 17:21:23 -0700

    enable a default validation test

--- test/components/header.test.ts
+++ test/components/header.test.ts
@@ -181,13 +181,9 @@ describe('Header component', () => {
         expect(error).to.equal(undefined)
       })
 
-      it.skip('value must match the schema', () => {
+      it.only('value must match the schema', () => {
         const [error] = Header2.validate({ type: 'string', default: 1 })
         expect(error).to.match(/Value does not match schema/)
-      })
-
-      it('will warn if default does not match schema', () => {
-        throw Error('to do')
       })
     })
   })
