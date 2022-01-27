76ebffdf819db5528b8131ec48a2336fa4a0002d
James Speirs <james.speirs@gmail.com>
2022-01-27 16:37:23 -0700

    set up esbuild config for node and browser

---
+++ esbuild.config.js
@@ -0,0 +1,77 @@
+const esbuild = require('esbuild')
+const fs = require('fs/promises')
+const path = require('path')
+
+// Automatically exclude all node_modules from the bundled version
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+const testDir = path.resolve(__dirname, 'src', 'test')
+const directive = process.argv[2] ?? ''
+
+const start = Date.now()
+;(async () => {
+  // nodejs build
+  if (directive !== 'browser') {
+    await esbuild.build({
+      outdir: './dist/node',
+      entryPoints: ['./src/library/index.ts'],
+      bundle: true,
+      minify: false,
+      keepNames: true,
+      platform: 'node',
+      sourcemap: true,
+      target: 'node16',
+      plugins: [nodeExternalsPlugin()]
+    })
+
+    // tests build
+    await buildTestFiles(testDir)
+  }
+
+  // browser build
+  if (directive !== 'node') {
+    await esbuild.build({
+      outdir: './dist/browser',
+      entryPoints: ['./src/index.ts'],
+      external: ['http', 'https', 'os', 'path', 'util'],
+      bundle: true,
+      minify: true,
+      keepNames: true,
+      platform: 'browser',
+      sourcemap: true,
+      target: 'es2020',
+      plugins: [nodeExternalsPlugin()]
+    })
+  }
+})()
+  .then(() => {
+    console.log('Build in ' + (Date.now() - start) + ' milliseconds')
+  })
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+
+async function buildTestFiles (dirPath) {
+  const filePaths = await fs.readdir(dirPath)
+  const promises = filePaths.map(async filePath => {
+    const fullPath = path.resolve(dirPath, filePath)
+    const stats = await fs.stat(fullPath)
+    if (stats.isDirectory()) {
+      await buildTestFiles(fullPath)
+    } else if (stats.isFile()) {
+      await esbuild.build({
+        outfile: path.resolve(__dirname, 'dist/tests', path.relative(testDir, fullPath)),
+        entryPoints: [fullPath],
+        bundle: true,
+        minify: false,
+        keepNames: true,
+        platform: 'node',
+        sourcemap: true,
+        target: 'node16',
+        plugins: [nodeExternalsPlugin()]
+      })
+    }
+  })
+  await Promise.all(promises)
+}
